0d533282047c40e9da413b1b4e56676b04944c27
Anton <kucherenko-anton1@mail.ru>
2023-08-23 18:02:03 +0300

    connectin with mongoose done

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 const express = require('express');
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 const app = express();
+
+const MONGO_DB_URI = process.env.MONGO_DB_URL;
 
 app.get('/', (req, res) => {
     res.send("Hello");
 })
 
-app.listen(3000);
+mongoose
+  .connect(MONGO_DB_URI)
+  .then((result) => {
+    console.log("Connected to the database successfully!");
+    app.listen(3000, () => {
+      console.log(`Server is running on -> http://localhost:3000`);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
